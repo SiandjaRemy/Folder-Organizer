17011e1440eef248f4efbd73285767cb7ac38e0f
Remy Siandja <remysiandja@gmail.com>
2024-12-28 17:50:00 +0100

    Script to organize songs based on artist and albums almost completed

--- music_album_organizer.py
+++ music_album_organizer.py
@@ -1,6 +1,7 @@
 import os
 import shutil
-
+from tinytag import TinyTag
+import re
 
 # Specify the folder to organize
 
@@ -8,48 +9,76 @@ folder_to_organize = input("Please enter the path of the folder to organize: ")
 
 folder_to_organize = folder_to_organize
 
-# Define the categories and their respective file extensions
-# Feel free to modify this dictionary or add the extenstions you want
-categories = {
-    'Videos': ['.mp4', '.mkv', '.mov', '.avi', '.flv', '.wmv', '.mpg', '.3gp', '.webm', '.ogv', '.m4v'],
-    'Audio': ['.mp3', '.wav', '.ogg', '.flac', '.aac', '.wma', '.m4a'],
-    'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp', '.svg', '.ico'],
-    'Documents': ['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt', '.xls', '.xlsx', '.ppt', '.pptx', '.csv'],
-    'Archives': ['.zip', '.rar', '.tar', '.gz', '.7z', '.bz2', '.xz'],
-    'Code': ['.py', '.js', '.html', '.css', '.java', '.cpp', '.c', '.php', '.rb', '.swift', '.go', '.json', '.xml', '.sql'],
-    'Executables': ['.exe', '.app', '.dmg', '.msi', '.bat', '.sh', '.bin'],
-    'Fonts': ['.ttf', '.otf', '.woff', '.woff2'],
-    'CAD': ['.dwg', '.dxf'],
-    '3D Models': ['.obj', '.stl', '.fbx', '.blend']
-}
 
+# Define an array of the file extensions accepted
+accepted_file_types = ['.mp3', '.wav', '.ogg', '.flac', '.aac', '.wma', '.m4a']
+
+
+def sanitize_filename(filename):
+    """Removes or replaces invalid characters from a filename."""
+    return re.sub(r'[\\/*?"<>|:]', '', filename)
 
 def organize_album():
 
-    # Create folders for each category if they do not exist
-    for category in categories.keys():
-        category_path = os.path.join(folder_to_organize, category)
-        if not os.path.exists(category_path):
-            os.makedirs(category_path)
-
+    # Check music author and create a folder if it does not yet exist
     # Move files to the appropriate folders
     for filename in os.listdir(folder_to_organize):
         file_extension = os.path.splitext(filename)[1].lower()
         source_file = os.path.join(folder_to_organize, filename)
+        
 
         # Check if it's a file (not a directory)
         if os.path.isfile(source_file):
-            moved = False
-            for category, extensions in categories.items():
-                if file_extension in extensions:
-                    destination_folder = os.path.join(folder_to_organize, category)
-                    shutil.move(source_file, destination_folder)
-                    print(f'Moved: {filename} to {category}/')
-                    moved = True
-                    break
-            
-            if not moved:
-                print(f'File not categorized: {filename}')
+            try:
+                audio = TinyTag.get(source_file) 
+
+                # print("Title:" + audio.title) 
+                # print("Artist: " + audio.artist) 
+                # print("Genre:" + audio.genre) 
+                # print("Year Released: " + audio.year) 
+                # print("Bitrate:" + str(audio.bitrate) + " kBits/s") 
+                # print("Composer: " + str(audio.composer)) 
+                # print("Filesize: " + str(audio.filesize) + " bytes") 
+                # print("AlbumArtist: " + str(audio.albumartist)) 
+                # print("Album: " + str(audio.album))
+                # print("Duration: " + str(audio.duration) + " seconds") 
+                # print("TrackTotal: " + str(audio.track_total)) 
+                # print("#######################################################") 
+                moved = False
+                for extensions in accepted_file_types:
+                    
+                    if file_extension in extensions:
+                        try:
+                            # Check if a folder with the artist name already exist in the current directory.
+                            # If it does not, create it.
+                            artist_folder_path = os.path.join(folder_to_organize, audio.artist)
+                            artist_folder_path_for_creation = os.path.join(folder_to_organize, sanitize_filename(audio.artist))
+                            if not os.path.exists(artist_folder_path):
+                                os.makedirs(artist_folder_path_for_creation)
+                            main_destination_folder = os.path.join(folder_to_organize, audio.artist)
+                            
+                            sub_folder_name = f"{sanitize_filename(audio.album)} - {audio.year}"
+                            sub_folder_name = sub_folder_name.replace("?", " ")
+                            sub_destination_folder = os.path.join(main_destination_folder, sub_folder_name)
+                            
+                            # Check if a folder with the sub_folder_name already exist in the directory that carries the artist name.
+                            # If it does not, create it.
+                            if not os.path.exists(sub_destination_folder):
+                                os.makedirs(sub_destination_folder)
+                                
+                            shutil.move(source_file, sub_destination_folder)
+                            print(f'Moved: {filename} to {audio.artist}/{sub_folder_name}')
+                            moved = True
+                            break
+                        except Exception as e:
+                            print(f"Failed due to: {e}")
+                            break
+                
+                if not moved:
+                    print(f'File not categorized: {filename}')
+                    
+            except Exception as e:
+                print(f"Failed to get audio tags: {e}")
 
     print("Organization complete.")
 
